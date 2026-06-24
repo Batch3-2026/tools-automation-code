3459d7aa20380804be40604af6676fb675c12582
MannuA19 <mannmeet.ahir@gmail.com>
2026-06-24 15:39:52 +0530

    Tools Automation Code

---
+++ infra-module/iam.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_role" "iam_role" {
+  name = "${var.name}-role"
+
+  # Terraform's "jsonencode" function converts a - Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.name}-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.name}-role"
+  role = aws_iam_role.iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  count     = length(var.policy_name)
+  role       = aws_iam_role.iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/${var.policy_name[count.index]}"
+}
--- infra-module/main.tf
+++ infra-module/main.tf
@@ -1,15 +1,53 @@
+resource "aws_security_group" "infra-sg" {
+
+      name        = "${var.name}-sg"
+      description = "${var.name}-sg"
+
+      tags = {
+        Name = "allow_tls"
+      }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.infra-sg.id
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.infra-sg.id
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.infra-sg.id
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.infra-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 resource "aws_instance" "tool" {
   ami           = data.aws_ami.rhel9.image_id
   instance_type = var.instance_type
   key_pair      = var.key_pair
+  vpc_security_group_ids = [aws_security_group.infra-sg.id]
+  iam_instance_profile = [aws_iam_instance_profile.instance_profile.name]
 
   # Reads the script relative to the configuration folder
   user_data_base64 = base64encode(templatefile("${path.module}/userdata.sh", {
     role_name = var.name
   }
-}
+    tags = {
+      Name = var.name
+    }
 
-  tags = {
-    Name = "${var.name}"
-  }
 }
